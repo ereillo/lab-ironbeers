216830a99b1fa1720cd5fecda5354c35f91fe4a9
ereillo <evelioreillo@gmail.com>
2023-08-02 18:37:18 +0200

    tercer salvado

--- app.js
+++ app.js
@@ -33,4 +33,16 @@ app.get('/beers', (req, res, next) => {
     .catch(error => console.log(error));
 });
 
+app.get("/random-beer", (req, res, next) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render("random-beer.hbs", {
+      responseFromAPI
+    })
+    console.log(responseFromAPI)
+  })
+  .catch(error => console.log(error));
+})
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
